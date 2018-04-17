bb2b9b80b8a19838dd14e8d8a3564a947455c581
nira <nayzaa2010@hotmail.com>
2018-04-17 17:36:04 +0700

    Android: RecipeActivity with chocolate pudding ID

--- app/src/androidTest/java/workshop/testing/ui/recipe/RecipeActivityTest.java
+++ app/src/androidTest/java/workshop/testing/ui/recipe/RecipeActivityTest.java
@@ -1,5 +1,6 @@
 package workshop.testing.ui.recipe;
 
+import android.content.Intent;
 import android.support.test.rule.ActivityTestRule;
 
 import org.junit.Rule;
@@ -12,6 +13,7 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
+import static workshop.testing.ui.recipe.RecipeActivity.KEY_ID;
 
 /**
  * Created by nayza on 17/4/2561.
@@ -19,12 +21,30 @@ import static org.junit.Assert.*;
 public class RecipeActivityTest {
 
     @Rule
-    public ActivityTestRule<RecipeActivity> activity = new ActivityTestRule(RecipeActivity.class);
+    public ActivityTestRule<RecipeActivity> activity = new ActivityTestRule(RecipeActivity.class, true, false);
 
     @Test
     public void openActivityByDefaultShouldShowRecipeNotFound(){
 
         onView(withId(R.id.description)).check(matches(withText("Recipe not found.")));
+
+    }
+
+    @Test
+    public void openActivityWithChocolatePuddingIDShouldShowRecipeForChocolatePudding(){
+
+        Intent intent = new Intent();
+        intent.putExtra(KEY_ID,"chocolate_pudding");
+        activity.launchActivity(intent);
+
+        onView(withId(R.id.description)).check(matches(withText("2 tablespoons chocolate\n" +
+                "yolks of 4 eggs\n" +
+                "1 cup sugar\n" +
+                "1 quart milk\n" +
+                "1 whole egg\n" +
+                "2 tablespoons corn starch\n" +
+                "\n" +
+                "Cook until it thickens, beat whites of eggs and put on top, put in oven to brown. Serve with cream, if preferred.")));
 
     }
 
